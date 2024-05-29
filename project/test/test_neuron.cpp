62ef8c4265f2aa1968b56d0bfc6553ecc7bfaef3
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2024-05-29 20:58:55 +0200

    muffi

--- project/test/test_neuron.cpp
+++ project/test/test_neuron.cpp
@@ -2,9 +2,3 @@
 #include "neuron.h"
 
 
-// Test fixture for Neuron class
-class NeuronTest : public ::testing::Test {
-protected:
-    Neuron neuron = Neuron(2, 0); // Create an instance of Neuron for testing
-};
-
