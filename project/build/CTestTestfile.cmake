2bfc0ce2104233ff13df64a7ce84d12c317c88da
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2024-05-29 21:02:32 +0200

    gitignore now should works

--- project/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kaczok/Desktop/neural-network/project
-# Build directory: C:/Users/kaczok/Desktop/neural-network/project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
