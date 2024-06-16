f7d8f909c661f4f6a9d45c6e0c549c30182ce9b7
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2024-06-16 14:03:24 +0200

    muffi

--- project/src/layers/sigmoid_layer.cpp
+++ project/src/layers/sigmoid_layer.cpp
@@ -1,4 +1,5 @@
 #include "sigmoid_layer.h"
+#include <cmath>
 
 void SigmoidLayer::feedForwardLayer(const std::shared_ptr<Layer>& prevLayer) {
     for (unsigned n = 0; n < size() - 1; ++n) {
@@ -37,7 +38,7 @@ void SigmoidLayer::backPropagation(std::shared_ptr<Layer>& prevLayer) {
 }
 
 double SigmoidLayer::activationFunction(double x)  {
-    return 1.0 / (1.0 + exp(-x)); 
+    return 1.0 / (1.0 + std::exp(-x)); 
 }
 
 double SigmoidLayer::activationFunctionDerivative(double x)  {
